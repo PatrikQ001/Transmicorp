8d48e49650b1cba4a728e03ac4f06e6dd676a349
AdrianCheje <40642400+AdrianCheje@users.noreply.github.com>
2023-11-29 12:29:09 -0500

    algunas correcciones

--- ProyectoTransmicorp/unidades/models.py
+++ ProyectoTransmicorp/unidades/models.py
@@ -7,11 +7,18 @@ ESTADO_UNIDAD = [
     ('En Tránsito', 'En Tránsito')
 ]
 
-PLACA_REGEX = r'^[A-Za-z0-9]{3}\d{3}$'
+TIPO_UNIDAD =[
+    ('Camión articulado', 'Camión articulado'),
+    ('Camión rígido', 'Camión rígido'),
+    ('Camión cerrado', 'Camión cerrado'),
+    ('Camión contenedor', 'Camión contenedor')
+]
+
+
 
 class unidades(models.Model):
-    Placa_Vehiculo = models.CharField(max_length=100, validators=[RegexValidator(PLACA_REGEX, 'Ingrese una placa válida.')])
-    Tipo_de_Vehiculo = models.CharField(max_length=100)
+    Placa_Vehiculo = models.CharField(max_length=100)
+    Tipo_de_Vehiculo = models.CharField(max_length=100, choices=TIPO_UNIDAD)
     Capacidad_Carga = models.IntegerField()
     Estado_unidad = models.CharField(max_length=30, choices=ESTADO_UNIDAD)
     Observaciones = models.CharField(max_length=100)
